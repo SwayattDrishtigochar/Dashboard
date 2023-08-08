6d9c4667a7034fdfd9ddc3fda931ab52aa846e9d
sunnyvedwal <sunnyvedwal@gmail.com>
2023-08-08 11:57:00 +0530

    cookie debug

--- Server/utils/generateToken.js
+++ Server/utils/generateToken.js
@@ -1,15 +1,22 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = async (res, userId) => {
-  // Create a JWT with the user ID and an expiration of 1 day.
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET);
+  try {
+    // Create a JWT with the user ID and an expiration of 1 day.
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET);
 
-  // Set a cookie with the JWT.
-  res.cookie('jwt_token', token, {
-    secure: true,
-    httpOnly: true,
-    SameSite: 'none',
-  });
+    // Set a cookie with the JWT.
+    res.cookie('jwt_token', token, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'none',
+    });
+  } catch (error) {
+    // Handle any errors that might occur during token generation or cookie setting
+    console.error('Error generating token:', error);
+    // You can choose to throw the error or handle it in some other way
+    throw new Error('Error generating token: ' + error.message);
+  }
 };
 
 export default generateToken;
