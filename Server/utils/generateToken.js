4c7aa57618b6ffb5b51aa2c9b8dbb1fd735cca35
sunnyvedwal <sunnyvedwal@gmail.com>
2023-08-28 12:59:57 +0530

    set expiry date for cookie

--- Server/utils/generateToken.js
+++ Server/utils/generateToken.js
@@ -3,13 +3,16 @@ import jwt from 'jsonwebtoken';
 const generateToken = async (res, userId) => {
   try {
     // Create a JWT with the user ID and an expiration of 1 day.
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET);
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
 
     // Set a cookie with the JWT.
     await res.cookie('jwt_token', token, {
       secure: true,
       httpOnly: true,
       sameSite: 'none',
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
     });
   } catch (error) {
     // Handle any errors that might occur during token generation or cookie setting
