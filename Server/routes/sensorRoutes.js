ec1dbf268120687a090b17aa18b1c514838a90a0
sunnyvedwal <sunnyvedwal@gmail.com>
2023-11-02 13:11:47 +0530

    ..

--- Server/routes/sensorRoutes.js
+++ Server/routes/sensorRoutes.js
@@ -1,13 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import {
-  getRunningHours,
-  getSensorData,
-  getStatus,
-} from '../controllers/sensorData.js';
+import { getSensorData, getStatus } from '../controllers/sensorData.js';
 
 router.get('/sensor', getSensorData);
 router.get('/sensor/status', getStatus);
-router.get('/sensor/runningtime', getRunningHours);
 
 export default router;
