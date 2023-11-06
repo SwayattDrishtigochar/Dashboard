491d88c8bf8f490de965a271e3d54a7a929c3d42
sunnyvedwal <sunnyvedwal@gmail.com>
2023-11-06 13:15:14 +0530

    fixed timezone issue

--- Server/controllers/sensorData.js
+++ Server/controllers/sensorData.js
@@ -32,8 +32,6 @@ const getSensorData = asyncHandler(async (req, res) => {
     if (data.documents.length > 0) {
       data.documents = data.documents.map((data) => {
         const dateIST = new Date(data.timestamp).toLocaleString('en-IN', {
-          timeZoneName: 'short',
-
           hour: '2-digit',
           minute: '2-digit',
         });
